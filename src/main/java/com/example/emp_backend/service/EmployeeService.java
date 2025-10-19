b688290b0056af6a5d478afa87719ab659251f21
Monica P <monicaspgowda@gmail.com>
2025-10-19 14:39:30 +0530

    Added createEmployee Logic

--- src/main/java/com/example/emp_backend/service/EmployeeService.java
+++ src/main/java/com/example/emp_backend/service/EmployeeService.java
@@ -1,4 +1,8 @@
 package com.example.emp_backend.service;
 
+import com.example.emp_backend.dto.EmployeeDto;
+
 public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+    EmployeeDto getEmployeeById(Long employeeId);
 }
