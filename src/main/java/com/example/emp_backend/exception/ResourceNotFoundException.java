25a416666c5313d6b7ae7577ba69cef61edece49
Monica P <monicaspgowda@gmail.com>
2025-10-21 13:03:53 +0530

    Updated ResourceNotFoundException file

--- src/main/java/com/example/emp_backend/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/emp_backend/exception/ResourceNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value= HttpStatus.NOT_FOUND)
-public class ResourceNotFooundException extends RuntimeException{
-    public ResourceNotFooundException(String message){
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
         super(message);
     }
 }
