d8250ec30a07620e9817578d922a6916b0e17bcc
Monica P <monicaspgowda@gmail.com>
2025-10-22 00:06:01 +0530

    Added createEmployee URL

--- emp_frontend/src/services/EmployeeService.jsx
+++ emp_frontend/src/services/EmployeeService.jsx
@@ -2,3 +2,4 @@ import axios from "axios";
 
 const REST_API_BASE_URL = 'http://localhost:8080/api/employees';
 export const listEmmployees = () => axios.get(REST_API_BASE_URL);
+export const createEmployee = (employee) => axios.post(REST_API_BASE_URL, employee);
