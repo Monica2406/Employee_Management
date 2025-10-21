3f2cf18f8882c8b6a4ff31c6ef1ffd618ad4ff31
Monica P <monicaspgowda@gmail.com>
2025-10-21 21:36:05 +0530

    Added path to add-employee

--- emp_frontend/src/App.jsx
+++ emp_frontend/src/App.jsx
@@ -1,8 +1,10 @@
 
 import './App.css'
+import EmployeeComponent from './components/EmployeeComponent'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 
 function App() {
@@ -11,15 +13,17 @@ function App() {
   return (
     <>
         <BrowserRouter>
-        <HeaderComponent/>
-          <Routes>
-            {/* // http://localhost:5173 */}
-            <Route path='/' element = { <ListEmployeeComponent/>}></Route>
-            {/* // http://localhost:5173/employees */}
-            <Route path='/employees' element = { <ListEmployeeComponent/>}></Route> 
-          </Routes>
-        <FooterComponent/>
-         </BrowserRouter>
+          <HeaderComponent/>
+            <Routes>
+              {/* // http://localhost:5173 */}
+              <Route path='/' element = { <ListEmployeeComponent/>}></Route>
+              {/* // http://localhost:5173/employees */}
+              <Route path='/employees' element = { <ListEmployeeComponent/>}></Route>
+              {/* // http://localhost:5173/add-employee */}
+              <Route path='/add-employee' element = {<EmployeeComponent/>}></Route> 
+            </Routes>
+          <FooterComponent/>
+        </BrowserRouter>
     
       
     </>
